1b02997955b3095882d772506526e028d9900206
Chetan Aggarwal <chetan15aggarwal@gmail.com>
2024-03-03 14:20:53 +0100

    Package Demo added

---
+++ DemoPackage-main/DemoPackageExample/DemoPackageExample/DemoPackageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoPackageExampleApp.swift
+//  DemoPackageExample
+//
+//  Created by Chetan Aggarwal on 14.12.22.
+//
+
+import SwiftUI
+
+@main
+struct DemoPackageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
